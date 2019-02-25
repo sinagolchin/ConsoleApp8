cedd2cd454f98f86f6cf2e0ce3c58f1d9089bfc4
sinagolchin <47105729+sinagolchin@users.noreply.github.com>
2019-02-25 13:33:27 +0330

    Add project files.

---
+++ ConsoleApp8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[11];
+            for (int k = 0; k <= 10; k++)
+            {
+                a[k] = k * 2;
+            }
+            for (int k = 0; k <= 10; k++)
+            {
+                Console.WriteLine(a[k]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
