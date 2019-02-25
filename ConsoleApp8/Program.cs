0ee5c9f8655ec8b5504e24cb9437453a2a4c7399
sinagolchin <47105729+sinagolchin@users.noreply.github.com>
2019-02-25 13:35:25 +0330

    adding a for loop and comment it

--- ConsoleApp8/Program.cs
+++ ConsoleApp8/Program.cs
@@ -8,18 +8,18 @@ namespace ConsoleApp8
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            int[] a = new int[11];
-            for (int k = 0; k <= 10; k++)
-            {
-                a[k] = k * 2;
-            }
-            for (int k = 0; k <= 10; k++)
-            {
-                Console.WriteLine(a[k]);
-            }
-            Console.ReadLine();
-        }
+        //static void Main(string[] args)
+        //{
+        //    int[] a = new int[11];
+        //    for (int k = 0; k <= 10; k++)
+        //    {
+        //        a[k] = k * 2;
+        //    }
+        //    for (int k = 0; k <= 10; k++)
+        //    {
+        //        Console.WriteLine(a[k]);
+        //    }
+        //    Console.ReadLine();
+        //}
     }
 }
